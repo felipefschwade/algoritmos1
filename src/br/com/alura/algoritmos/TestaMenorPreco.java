b48692dfbff27346c42b1c5bd90ee01acb580f3c
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-01-11 22:57:39 -0300

    first commit

---
+++ src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -0,0 +1,23 @@
+package br.com.alura.algoritmos;
+
+public class TestaMenorPreco {
+	public static void main(String[] args){
+		 double precos[] = new double[5];
+         precos[0] = 1000000;
+         precos[1] = 46000;
+         precos[2] = 16000;
+         precos[3] = 46000;
+         precos[4] = 17000;
+
+         int maisBarato = 0;
+
+        // pseudo código, não compila 
+         for(int atual = 0; atual < precos.length; atual++){
+             if (precos[atual] < maisBarato){
+                 maisBarato = atual;
+             }
+         }
+         System.out.println(maisBarato);
+         System.out.println("O carro mais barato custa " + precos[maisBarato]);
+	}
+}
