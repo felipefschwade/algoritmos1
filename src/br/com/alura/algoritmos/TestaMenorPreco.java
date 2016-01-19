defd5f5e2f5f778d527d6a363b29c83c5a89b49f
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-01-18 23:27:29 -0300

    Algoritmos 1 - Atividade 1 - Procurando em uma faixa específica do array

--- src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -2,20 +2,24 @@ package br.com.alura.algoritmos;
 
 public class TestaMenorPreco {
 	public static void main(String[] args){
-		 Produto produtos[] = new Produto[5];
-		 produtos[0] = new Produto("Lamborghini", 1000000);
-         produtos[1] = new Produto("Jipe", 46000);
-         produtos[2] = new Produto("Brasilia", 16000);
-         produtos[3] = new Produto("Smart", 46000);
-         produtos[4] = new Produto("Fusca", 17000);
-
-         int maisBarato = 0;
-
-         for(int atual = 0; atual < produtos.length; atual++){
-             if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
-                 maisBarato = atual;
-             }
-         }
+		 Produto[] produtos = {
+				 new Produto("Lamborghini", 1000000),
+				 new Produto("Jipe", 46000),
+				 new Produto("Brasilia", 16000),
+				 new Produto("Smart", 46000),
+				 new Produto("Fusca", 17000)
+				 };
+		 int maisBarato = buscaMaisBarato(produtos, 0, 4);
          System.out.println("O carro mais barato é: "+ produtos[maisBarato].getNome() +" e custa " + produtos[maisBarato].getPreco());
+         }
+
+	private static int buscaMaisBarato(Produto[] produtos, int inicio, int termino) {
+		int maisBarato = inicio;
+        for(int atual = inicio; atual <= termino; atual++){
+            if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
+                maisBarato = atual;
+            }
+        }
+        return maisBarato;
 	}
 }
