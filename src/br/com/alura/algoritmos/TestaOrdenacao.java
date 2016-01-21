d640ad7f092f67fac9b3622dc645610b3ea5f7a6
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-01-21 00:23:35 -0300

    Algoritmos 1 - Aula 29 - Criando um novo algoritmo de ordenação utilizando o método insertionSort

--- src/br/com/alura/algoritmos/TestaOrdenacao.java
+++ src/br/com/alura/algoritmos/TestaOrdenacao.java
@@ -9,15 +9,31 @@ public class TestaOrdenacao {
 				 new Produto("Smart", 46000),
 				 new Produto("Fusca", 17000)
 				 };
-		for (int atual = 0; atual <= produtos.length - 1; atual++) {
-			int maisBarato = buscaMaisBarato(produtos, atual, produtos.length - 1);
+		//selectionSort(produtos, produtos.length);
+		novoSort(produtos, produtos.length);
+		for (Produto produto : produtos){
+			System.out.println(produto.getNome() + " Custa: " + produto.getPreco());
+		}
+	}
+	private static void novoSort(Produto[] produtos, int length) {
+		for (int atual = 0; atual < produtos.length; atual++) {
+			int analise = atual;
+			while (analise > 0 && produtos[analise].getPreco() < produtos[analise - 1].getPreco()) {
+				Produto produtoAnalise = produtos[analise];
+				Produto produtoAnterior = produtos[analise - 1];
+				produtos[analise - 1] = produtoAnalise;
+				produtos[analise] = produtoAnterior;
+				analise--;
+			}
+		}
+	}
+	private static void selectionSort(Produto[] produtos, int quantidadeDeElementos) {
+		for (int atual = 0; atual < quantidadeDeElementos - 1; atual++) {
+			int maisBarato = buscaMaisBarato(produtos, atual,quantidadeDeElementos- 1);
 			Produto produtoAtual = produtos[atual];
 			Produto produtoMaisBarato = produtos[maisBarato];
 			produtos[atual] = produtoMaisBarato;
 			produtos[maisBarato] = produtoAtual;
-		}
-		for (Produto produto : produtos){
-			System.out.println(produto.getNome() + " Custa: " + produto.getPreco());
 		}
 	}
 	private static int buscaMaisBarato(Produto[] produtos, int inicio, int termino) {
